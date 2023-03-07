b2e458bf27c5a0562099c5ddf76a8fa1328dfffb
danieldiasm <daniel_diasm@hotmail.com>
2023-03-07 22:11:36 +0100

    refactoring

---
+++ movie_list/terminal_app/app.py
@@ -0,0 +1,15 @@
+import model
+import menu
+
+
+if __name__ == '__main__':
+    while (user_input := input(menu.menu_main)) != "4":
+        print(user_input)
+        if user_input == "1":
+            print("You've selected 1")
+        elif user_input == "2":
+            print("You've selected 2")
+        elif user_input == "3":
+            print("You've selected 3")
+        else:
+            print("Invalid user_input!")
---
+++ movie_list/terminal_app/menu.py
@@ -0,0 +1,16 @@
+
+menu_main = """
+1 - View movie list.
+2 - Add new movie.
+3 - Edit movie.
+4 - Exit
+
+Selected:"""
+
+menu_view = """
+1 - List Watched.
+2 - List Unwatched.
+3 - List All.
+4 - Return to main
+
+Selected:"""
