7bfc3b8d54a100f52548309e77f378a6b19c6389
danieldiasm <daniel_diasm@hotmail.com>
2023-03-08 21:40:14 +0100

    created a wrapper as decorator for db conn

--- movie_list/terminal_app/model.py
+++ movie_list/terminal_app/model.py
@@ -1,4 +1,5 @@
 import sqlite3
+import functools
 import os
 import queries
 
@@ -10,33 +11,35 @@ class database:
         if not os.path.isfile(self.dbfile):
             sqlite3.connect(self.dbfile)
             self.initialize_db()
-       
+
+    def with_db(func):
+        @functools.wraps(func)
+        def dbconn_wrapper(self, query):
+            with sqlite3.connect(self.dbfile) as conn:
+                cursor = conn.cursor()
+                result = func(cursor, query)
+                cursor.close()
+            return result
+        return dbconn_wrapper
+
 
     def initialize_db(self) -> bool:
-        if database.execute(self, queries.create_table_movies):
-            return True
-        else:
-            raise Exception("Failed creating tables, sorry.")
+        try:
+            self.exec_only(queries.create_table_movies)
+            # Add here other actions needed to init the db
+        except Exception as e:
+            return e
 
-    def execute(self, query):
-        with sqlite3.connect(self.dbfile) as conn:
-                cursor = conn.cursor()
-                try:
-                    cursor.execute(query)
-                except Exception as e:
-                    raise f"Something went wrong:{e}"
-                    return False
-                cursor.close()
-        return True
+
+    @with_db
+    def exec_only(self, cursor, query) -> None:
+        cursor.execute(query)
+        return
+
+
+    @with_db
+    def exec_fetchall(self, cursor, query) -> str:
+        cursor.execute(query)
+        query_result = cursor.fetchall  
+        return query_result
     
-    def fetch_all(self, query):
-        with sqlite3.connect(self.dbfile) as conn:
-                cursor = conn.cursor()
-                try:
-                    cursor.execute(query)
-                    result = cursor.fetchall()
-                except Exception as e:
-                    raise f"Something went wrong:{e}"
-                    return False
-                cursor.close()
-        return result
