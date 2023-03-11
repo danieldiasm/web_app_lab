b7f4d7bd9d12523069832e42c1ebe955d5510a92
danieldiasm <daniel_diasm@hotmail.com>
2023-03-11 20:29:06 +0100

    Added unittests to inst and db init

--- movie_list/terminal_app/tests/unit/test_model.py
+++ movie_list/terminal_app/tests/unit/test_model.py
@@ -1,18 +1,44 @@
 from unittest import TestCase
-from unittest.mock import MagicMock
+from unittest.mock import patch
 import os
 
 import model as model
+import queries
 
-class TestModelMocked(TestCase):
+class TestModelInstance(TestCase):
 
-    def test_create_db(self):
-        model.initialize_db = MagicMock()
-        model.initialize_db.return_value = None
+    def setUp(self) -> None:
+        self.patcher1 = patch('model.Database.initialize_db')
+        self.MockClass1 = self.patcher1.start()
 
-        moviedb = model.database()
+    def tearDown(self) -> None:
+        self.patcher1.stop()
 
-        expected_result = True
-        actual_result = os.path.isfile(moviedb.dbfile)
+    def test_create_class(self):
+        
+        movie_list_model = model.Database()
+        self.assertTrue(isinstance(movie_list_model, model.Database))
 
-        self.assertEqual(actual_result, expected_result, msg="Failed creating DB File!")
+class TestModelInit(TestCase):
+
+    def setUp(self) -> None:
+        self.patcher1 = patch('model.Database.exec_only')
+        self.MockClass1 = self.patcher1.start()
+
+    def tearDown(self) -> None:
+        self.patcher1.stop()
+
+    def test_initialize_db(self) -> None:
+        movie_list_model = model.Database()
+        movie_list_model.initialize_db()
+
+        # Assert if initialize db was called with query
+        self.MockClass1.assert_called_with(queries.create_table_movies)
+
+class TestExecMethods(self):
+
+    def test_exec_only(self):
+        pass
+
+    def test_exec_fetchall(self):
+        pass
